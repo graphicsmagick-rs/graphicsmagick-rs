fa9d62b299a3719c2932cf46692fbebbc4296bed
jmjoy <918734043@qq.com>
2022-02-07 21:45:47 +0800

    Update rust edition, crate version and document.

--- graphicsmagick/src/lib.rs
+++ graphicsmagick/src/lib.rs
@@ -3,68 +3,7 @@
 #![warn(clippy::dbg_macro, clippy::print_stdout)]
 #![allow(clippy::too_many_arguments)]
 #![allow(unused_doc_comments)]
-//! <img src="https://github.com/jmjoy/graphicsmagick-rs/blob/master/meta/GraphicsMagick-Logo.webp?raw=true" alt="GraphicsMagick-Logo" align="right" />
-//!
-//! GraphicsMagick binding for Rust.
-//!
-//! ## Requirement
-//!
-//! Require `graphicsmagick`, `libgraphicsmagick`, `clang` and `libclang`.
-//!
-//! In Deepin/Ubuntu/Debian, you can install these by:
-//!
-//! ```bash
-//! sudo apt install graphicsmagick libgraphicsmagick1-dev
-//! sudo apt install llvm-dev libclang-dev clang
-//! ```
-//!
-//! Before build, please check the `GraphicsMagickWand-config` is executable,
-//! or specify the environment variable `GRAPHICS_MAGICK_WAND_CONFIG` correctly.
-//!
-//! ## Support
-//!
-//! 1. Support and tested GraphicsMagick version: `1.3.20 ~ 1.3.36`.
-//!
-//! 1. There are some version flag in the documentation, like `gm_v_1_3_26`, meaning that your `GraphicsMagick` version must
-//!    be greater than or equal to `1.3.26` to used this method.
-//!
-//!    ![version-demo](https://github.com/jmjoy/graphicsmagick-rs/blob/master/meta/version-demo.webp?raw=true)
-//!    
-//! 1. `GraphicsMagick` supports OpenMP if you are compiling with OpenMP-enabled `cc`, you can set the environment variable
-//!    `OMP_NUM_THREADS` to limit the number of threads or set `OMP_DISPLAY_ENV=TRUE` to display the OpenMP info when
-//!    running the application.
-//!
-//!    Read <http://www.graphicsmagick.org/OpenMP.html> for details.
-//!
-//! ## Example
-//!
-//! Simple resize example:
-//!
-//! ```rust
-//! use anyhow::Context;
-//! use graphicsmagick::{initialize, types::FilterTypes, wand::MagickWand};
-//! use std::path::PathBuf;
-//!
-//! fn main() -> anyhow::Result<()> {
-//!     // This function should be invoked in the primary (original) thread
-//!     // of the application's process, and before starting any OpenMP
-//!     // threads, as part of program initialization.
-//!     initialize();
-//!
-//!     let path = PathBuf::from(env!("CARGO_MANIFEST_DIR"))
-//!         .join("..")
-//!         .join("meta")
-//!         .join("GraphicsMagick-Logo.webp");
-//!     let path = path.to_str().context("get image path failed")?;
-//!
-//!     let mut mw = MagickWand::new();
-//!     mw.read_image(path)?
-//!         .resize_image(100, 100, FilterTypes::UndefinedFilter, 1.)?
-//!         .write_image("/tmp/output.webp")?;
-//!
-//!     Ok(())
-//! }
-//! ```
+#![doc = include_str!("../README.md")]
 
 #[macro_use]
 mod macros;
@@ -74,9 +13,6 @@ pub(crate) mod tests;
 pub mod types;
 pub(crate) mod utils;
 pub mod wand;
-
-#[cfg(doctest)]
-doc_comment!(include_str!("../README.md"));
 
 pub use crate::{
     error::{Error, Result},
--- graphicsmagick/src/macros.rs
+++ graphicsmagick/src/macros.rs
@@ -44,15 +44,3 @@ macro_rules! types_enum_block {
         }
     }
 }
-
-#[cfg(doctest)]
-macro_rules! doc_comment {
-    ($x:expr) => {
-        #[doc = $x]
-        extern "C" {}
-    };
-    ($x:expr, $($tt:tt)*) => {
-        #[doc = $x]
-        $($tt)*
-    };
-}

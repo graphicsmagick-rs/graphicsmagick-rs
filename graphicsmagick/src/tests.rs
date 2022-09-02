f544501b85bb4d466208313b1a4931c8e87e7fbe
Jiahao XU <Jiahao_XU@outlook.com>
2022-09-02 15:33:27 +1000

    FIx clippy warning in mod `src/tests.rs`
    Signed-off-by: Jiahao XU <Jiahao_XU@outlook.com>

--- graphicsmagick/src/tests.rs
+++ graphicsmagick/src/tests.rs
@@ -1,11 +1,11 @@
 pub(crate) fn logo_path() -> String {
     let mut path = std::env::var("PWD").unwrap();
-    path.extend("/meta/GraphicsMagick-Logo.webp".chars());
+    path.push_str("/meta/GraphicsMagick-Logo.webp");
     path
 }
 
 pub(crate) fn logo_unicode_path() -> String {
     let mut path = std::env::var("PWD").unwrap();
-    path.extend("/meta/GraphicsMagick-图标.webp".chars());
+    path.push_str("/meta/GraphicsMagick-图标.webp");
     path
 }

580530467d5363013d099edf786a55ed38640a2e
Jiahao XU <Jiahao_XU@outlook.com>
2022-09-05 21:13:00 +1000

    Rm unused fn `str_to_c_string`
    Signed-off-by: Jiahao XU <Jiahao_XU@outlook.com>

--- graphicsmagick/src/utils/mod.rs
+++ graphicsmagick/src/utils/mod.rs
@@ -1,5 +1,3 @@
-use std::ffi::CString;
-
 mod init;
 pub(crate) use init::assert_initialized;
 pub use init::{has_initialized, initialize};
@@ -10,12 +8,3 @@ pub use rgb::{max_rgb, MaxRGB};
 mod magick_alloc;
 pub(crate) use magick_alloc::{CStrExt, MagickAutoRelinquish};
 pub use magick_alloc::{MagickBoxSlice, MagickCString};
-
-pub(crate) fn str_to_c_string(s: &str) -> CString {
-    let buf = s.bytes().collect::<Vec<_>>();
-    // from_vec_unchecked appends the trailing '\0'
-    //
-    // Safety:
-    // s is a utf-8 str, so it cannot contains '\0'.
-    unsafe { CString::from_vec_unchecked(buf) }
-}
